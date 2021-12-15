2a87f49286d240f1fe7f0ff28e7cd4eb413f6523
Lenovo <d.shparko@mail.ru>
2021-12-15 08:14:33 +0300

    add other files

---
+++ Faker/Main/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+using Faker;
+using Faker.BaseTypes;
+using System.Collections.Generic;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Faker.Faker f = new Faker.Faker();
+            User ff = f.Create<User>();
+            Console.WriteLine(JsonConvert.SerializeObject(ff,Formatting.Indented));
+        }
+    }
+}
